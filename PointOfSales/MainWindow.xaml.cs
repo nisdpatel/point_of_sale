e678c9bf2cec5bb03cc256d331ec28f5905bf4b1
nisdpate <nisdpate@ut.utm.edu>
2017-11-20 09:19:26 -0600

    Code added

--- PointOfSales/MainWindow.xaml.cs
+++ PointOfSales/MainWindow.xaml.cs
@@ -12,17 +12,54 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+
+
+
 
 namespace PointOfSales
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    /// </summ
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            Categories.Add(new Category { Name = "Books", Items = new List<string> { "Harry Potter and The Philosopher's Stone", "Cat and The Hat", "C# For Dummies" } });
+            Categories.Add(new Category { Name = "Electronics", Items = new List<string> { "Dell Laptop", "Iphone", "eReader" } });
+            Categories.Add(new Category { Name = "Homegoods", Items = new List<string> { "Microwave", "Vacuum", "Lamp" } });
+            Categories.Add(new Category { Name = "Groceries", Items = new List<string> { "Milk", "Bread", "Cereal" } });
+        }
+        private ObservableCollection<Category> categories = new ObservableCollection<Category>();
+        public ObservableCollection<Category> Categories
+        {
+            get { return categories; }
+            set { categories = value; }
+        }
+
+        private void AddCart_Click(object sender, RoutedEventArgs e)
+        {
+            Cart.Items.Add(Shop_Items.SelectedItem);
+        }
+
+        private void RemoveCart_Click(object sender, RoutedEventArgs e)
+        {
+            Cart.Items.Remove(Cart.SelectedItem);
+
+        }
+
+        private void EmptyCart_Click(object sender, RoutedEventArgs e)
+        {
+            Cart.Items.Clear();
         }
     }
+
+    public class Category
+    {
+        public string Name { get; set; }
+        public List<string> Items { get; set; }
+    }
+
 }
