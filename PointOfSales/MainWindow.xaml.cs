2a74bd0bb2db40caed3420b59eb10c4a50ce37cf
Joshua Cannon <Joshua Cannon@LAPTOP-S82MHRJ7>
2017-11-24 00:54:21 -0600

    revert back

--- PointOfSales/MainWindow.xaml.cs
+++ PointOfSales/MainWindow.xaml.cs
@@ -19,15 +19,56 @@ using System.IO;
 
 namespace PointOfSales
 {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summ
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
-            Product p = new Product("Name ", "Price", "Description");
-            Department d = new Department();
             InitializeComponent();
+            Categories.Add(new Category { Name = "Books", Items = new List<String> { "Harry Potter and The Philosopher's Stone", "Cat and The Hat", "C# For Dummies" } });
+            Categories.Add(new Category { Name = "Electronics", Items = new List<String> { "Dell Laptop", "Iphone", "eReader" } });
+            Categories.Add(new Category { Name = "Homegoods", Items = new List<String> { "Microwave", "Vacuum", "Lamp" } });
+            Categories.Add(new Category { Name = "Groceries", Items = new List<String> { "Milk", "Bread", "Cereal" } });
+        }
+        private ObservableCollection<Category> categories = new ObservableCollection<Category>();
+        public ObservableCollection<Category> Categories
+        {
+            get { return categories; }
+            set { categories = value; }
+        }
 
-            d.products.Add(p);
+        private void AddCart_Click(object sender, RoutedEventArgs e)
+        {
+            Cart.Items.Add(Shop_Items.SelectedItem);
+        }
+
+        private void RemoveCart_Click(object sender, RoutedEventArgs e)
+        {
+            Cart.Items.Remove(Cart.SelectedItem);
+
+        }
+
+        private void EmptyCart_Click(object sender, RoutedEventArgs e)
+        {
+            Cart.Items.Clear();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
- }
+
+    public class Category
+    {
+        public string Name { get; set; }
+        public List<String> Items { get; set; }
+
+
+
+    }
+
+
+}
