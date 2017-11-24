fd381cd5b5b06addb68108e663f77cd53408f10a
Joshua Cannon <Joshua Cannon@LAPTOP-S82MHRJ7>
2017-11-24 17:47:57 -0600

    update products and categories

--- PointOfSales/MainWindow.xaml.cs
+++ PointOfSales/MainWindow.xaml.cs
@@ -26,17 +26,25 @@ namespace PointOfSales
     {
         public MainWindow()
         {
+
+
+            Product harry_potter = new Product("Harry Potter and the Philosopher's Stone", "26.00", "First Book in the Harry Potter Series");
+            List<Product> new_products = new List<Product> { harry_potter };
+
+            Category books = new Category("Books", new_products);
+
+
             InitializeComponent();
-            Categories.Add(new Category { Name = "Books", Items = new List<String> { "Harry Potter and The Philosopher's Stone", "Cat and The Hat", "C# For Dummies" } });
-            Categories.Add(new Category { Name = "Electronics", Items = new List<String> { "Dell Laptop", "Iphone", "eReader" } });
-            Categories.Add(new Category { Name = "Homegoods", Items = new List<String> { "Microwave", "Vacuum", "Lamp" } });
-            Categories.Add(new Category { Name = "Groceries", Items = new List<String> { "Milk", "Bread", "Cereal" } });
+            Products.Add(books);
+            // Categories.Add(new Category { Name = "Electronics", Items = new List<String> { "Dell Laptop", "Iphone", "eReader" } });
+            // Categories.Add(new Category { Name = "Homegoods", Items = new List<String> { "Microwave", "Vacuum", "Lamp" } });
+            // Categories.Add(new Category { Name = "Groceries", Items = new List<String> { "Milk", "Bread", "Cereal" } });
         }
-        private ObservableCollection<Category> categories = new ObservableCollection<Category>();
-        public ObservableCollection<Category> Categories
+        private ObservableCollection<Category> products = new ObservableCollection<Category>();
+        public ObservableCollection<Category> Products
         {
-            get { return categories; }
-            set { categories = value; }
+            get { return products; }
+            set { products = value; }
         }
 
         private void AddCart_Click(object sender, RoutedEventArgs e)
@@ -55,20 +63,46 @@ namespace PointOfSales
             Cart.Items.Clear();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        public class Category
         {
+            public string Cname { get; set; }
+            public List<Product> Cproducts { get; set; }
 
+            public Category(string cname, List<Product> cproducts)
+            {
+                this.Cname = cname;
+                this.Cproducts = cproducts;
+            }
+
+            public override string ToString()
+            {
+                return this.Cname;
+            }
+
+            // public ObservableCollection<Category> products = new ObservableCollection<Category>();
         }
+
+            public class Product
+            {
+                public string Pname { get; set; }
+                public string Pprice { get; set; }
+                public string Pdescription { get; set; }
+
+
+                public Product(string pname, string pprice, string pdescription)
+                {
+                    this.Pname = pname;
+                    this.Pprice = pprice;
+                    this.Pdescription = pdescription;
+
+                }
+
+                public override string ToString()
+                {
+                    return this.Pname + " | Price: $" + this.Pprice + " | Description: " + this.Pdescription;
+                }
+            }
+        
+
     }
-
-    public class Category
-    {
-        public string Name { get; set; }
-        public List<String> Items { get; set; }
-
-
-
-    }
-
-
 }
