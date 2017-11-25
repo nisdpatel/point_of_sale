c322e1ddb884c28815d48ae7023d391421fef883
Joshua Cannon <Joshua Cannon@LAPTOP-S82MHRJ7>
2017-11-24 20:44:18 -0600

    add comment

--- PointOfSales/MainWindow.xaml.cs
+++ PointOfSales/MainWindow.xaml.cs
@@ -19,25 +19,26 @@ using System.IO;
 
 namespace PointOfSales
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summ
     public partial class MainWindow : Window
     {
         public MainWindow()
-        {
+
+        {/*****creating new books and adding to book category ******/
             Product c_tutor = new Product("C# For Dummies", "54.78", "C# tutorial");
             Product cat_hat = new Product("Cat and the Hat", "2.00", "Children's book from Dr.Suess");
 
-            List<Product> new_products = new List<Product> {cat_hat};
-            new_products.Add(c_tutor);
+            List<Product> new_books = new List<Product> {cat_hat};
+            new_books.Add(c_tutor);
 
-            Category books = new Category("Books", new_products);
-
+            Category books = new Category("Books", new_books);
+        /**************************************************************/
 
             InitializeComponent();
-            Categories.Add(books);
+
+            Categories.Add(books);//adding book category to observable collection
         }
+
+        /************* creating our observable list and defining its getter and setter (listing is bounded to "category_box")************/
         private ObservableCollection<Category> categories = new ObservableCollection<Category>();
         public ObservableCollection<Category> Categories
         {
@@ -45,22 +46,42 @@ namespace PointOfSales
             set { categories = value; }
         }
 
+        /******************************************************************************************/
+
+        /************Button Events*****************************************************************/
+
         private void AddCart_Click(object sender, RoutedEventArgs e)
         {
-            Cart.Items.Add(Shop_Items.SelectedItem);
+            Cart.Items.Add(Shop_Items.SelectedItem);//add items to shopping cart
         }
 
         private void RemoveCart_Click(object sender, RoutedEventArgs e)
         {
-            Cart.Items.Remove(Cart.SelectedItem);
+            Cart.Items.Remove(Cart.SelectedItem);// remove selected item from shopping cart
 
         }
 
         private void EmptyCart_Click(object sender, RoutedEventArgs e)
         {
-            Cart.Items.Clear();
+            Cart.Items.Clear();//clear all items from shopping cart
         }
 
+        private void ChckOutBtn_Click(object sender, RoutedEventArgs e)
+        {
+            double subtotal = 0;
+
+            for (int i = 0; i < Cart.Items.Count; i++)
+            {
+                Product cartitem = (Product)Cart.Items[i];
+                subtotal += Convert.ToDouble(cartitem.Pprice);
+
+            }
+
+            MessageBox.Show("Sub-Total: $" + subtotal.ToString());//display subtotal for all items in shopping cart
+        }
+        //*******************************************************************/
+
+        /*****************Custom category and product classes****************/
         public class Category
         {
             public string Cname { get; set; }
@@ -104,18 +125,6 @@ namespace PointOfSales
                   return this.Pname + " | Price: $" + this.Pprice + " | Description: " + this.Pdescription;
               }
         }
-
-        private void ChckOutBtn_Click(object sender, RoutedEventArgs e) {
-          double subtotal = 0;
-
-          for(int i = 0; i < Cart.Items.Count ; i++)
-          {
-              Product cartitem = (Product)Cart.Items[i];
-              subtotal += Convert.ToDouble(cartitem.Pprice);
-                    
-          }
-
-          MessageBox.Show("Sub-Total: $" + subtotal.ToString());
-        }
+        /***********************************************************************************************************/
     }
 }
