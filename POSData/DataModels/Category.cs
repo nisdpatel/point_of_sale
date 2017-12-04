8fae04891da079cd0251e0ee48351261078dcfc1
Joshua Cannon <Joshua Cannon@LAPTOP-S82MHRJ7>
2017-12-03 23:04:04 -0600

    added database functionalit

---
+++ POSData/DataModels/Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Collections.ObjectModel;
+
+namespace POSData
+{
+    public class Category
+    {
+        public Category()
+        {
+            this.Products = new ObservableCollection<Product>();
+        }
+
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public string TaxRate { get; set; }
+
+        public virtual ObservableCollection<Product> Products { get; private set; }
+    }
+}
