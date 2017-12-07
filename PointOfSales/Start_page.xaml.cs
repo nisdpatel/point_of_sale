60c7b386436c82213a591db6a687c850196c4652
Joshua Cannon <Joshua Cannon@LAPTOP-S82MHRJ7>
2017-12-06 21:02:26 -0600

    added login screen

---
+++ PointOfSales/Start_page.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PointOfSales
+{
+    /// <summary>
+    /// Interaction logic for Start_page.xaml
+    /// </summary>
+    public partial class Start_page : Window
+    {
+        public Start_page()
+        {
+            InitializeComponent();
+        }
+        private void admin_btn_Click(object sender, RoutedEventArgs e)
+        {
+            Checkout chkout = new Checkout();
+            chkout.Show();
+            this.Close();
+
+
+        }
+
+        private void user_btn_Click(object sender, RoutedEventArgs e)
+        {
+            Checkout chkout = new Checkout();
+            chkout.inventory.Visibility = Visibility.Hidden;
+            chkout.Show();
+
+            this.Close();
+
+        }
+    }
+}
